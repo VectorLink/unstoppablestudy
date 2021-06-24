d74d2a4ce522f422c16bad269c26e9e3fa959603
lijun <lijun@lightinthebox.com>
2021-06-24 21:28:11 +0800

    Initial commit

---
+++ src/main/java/com/unstoppable/unstoppablestudy/UnstoppablestudyApplication.java
@@ -0,0 +1,13 @@
+package com.unstoppable.unstoppablestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnstoppablestudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnstoppablestudyApplication.class, args);
+	}
+
+}
