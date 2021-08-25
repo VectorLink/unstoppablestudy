a6cfbea8eeffd8182f0dae5a30ced6475c12eaa6
VectorLink <1107746668@qq.com>
2021-08-25 22:53:10 +0800

    接入consul

--- src/main/java/com/unstoppable/unstoppablestudy/UnstoppablestudyApplication.java
+++ src/main/java/com/unstoppable/unstoppablestudy/UnstoppablestudyApplication.java
@@ -2,12 +2,16 @@ package com.unstoppable.unstoppablestudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @EnableTransactionManagement
 @EnableOpenApi
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class UnstoppablestudyApplication {
 
 	public static void main(String[] args) {
