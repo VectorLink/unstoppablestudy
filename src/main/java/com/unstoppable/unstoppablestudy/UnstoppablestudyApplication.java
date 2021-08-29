58573ea36a83f2f5823a6e03a5985c26e21ec26f
VectorLink <1107746668@qq.com>
2021-08-29 15:35:04 +0800

    增加netty测试单元

--- src/main/java/com/unstoppable/unstoppablestudy/UnstoppablestudyApplication.java
+++ src/main/java/com/unstoppable/unstoppablestudy/UnstoppablestudyApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ApplicationContext;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import springfox.documentation.oas.annotations.EnableOpenApi;
 
@@ -14,8 +15,13 @@ import springfox.documentation.oas.annotations.EnableOpenApi;
 @EnableConfigurationProperties
 public class UnstoppablestudyApplication {
 
+	public static ApplicationContext applicationContext;
+
 	public static void main(String[] args) {
 		SpringApplication.run(UnstoppablestudyApplication.class, args);
 	}
 
+	public static ApplicationContext getApplicationContext() {
+		return applicationContext;
+	}
 }
